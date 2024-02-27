f0aa01b0d513efd41051954dbf18b2f905479aa4
44 SCIENTIFICS <156848151+44Scientifics@users.noreply.github.com>
2024-02-27 12:02:38 +0000

    27 feb 2024
    27 feb 2024

---
+++ src/FortyFour/Utils/aws.py
@@ -0,0 +1,25 @@
+def upload_to_S3(file_object, bucket_name: str, file_name: str):
+    s3 = boto3.client(
+        service_name="s3",
+        region_name=st.secrets["AWS_DEFAULT_REGION"],
+        aws_access_key_id=st.secrets["AWS_ACCESS_KEY_ID"],
+        aws_secret_access_key=st.secrets["AWS_SECRET_ACCESS_KEY"]
+    )
+
+    s3.upload_fileobj(file_object, bucket_name, file_name)
+
+    file_path_in_s3 = f"{bucket_name}/{file_name}"
+    return file_path_in_s3
+
+
+
+def read_file_from_s3(bucket_name, file_name):
+    s3 = boto3.client(
+        service_name="s3",
+        region_name=st.secrets["AWS_DEFAULT_REGION"],
+        aws_access_key_id=st.secrets["AWS_ACCESS_KEY_ID"],
+        aws_secret_access_key=st.secrets["AWS_SECRET_ACCESS_KEY"]
+    )
+    obj = s3.get_object(Bucket=bucket_name, Key=file_name)
+    data = obj['Body'].read()
+    return data
