db6755f585f360fdc4138af8504afa2ce87d3c22
44 SCIENTIFICS <156848151+44Scientifics@users.noreply.github.com>
2024-02-29 13:14:12 +0000

    bug fix
    bug fix

--- src/FortyFour/Utils/aws.py
+++ src/FortyFour/Utils/aws.py
@@ -30,3 +30,13 @@ def read_file_from_s3(bucket_name, file_name):
     data = obj['Body'].read()
     return data
 
+
+def delete_file_from_S3(bucket_name, file_name):
+    s3 = boto3.client(
+        service_name="s3",
+        region_name=os.getenv("AWS_DEFAULT_REGION"),
+        aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
+        aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY")
+    )
+    obj = s3.get_object(Bucket=bucket_name, Key=file_name)
+    obj.delete()
