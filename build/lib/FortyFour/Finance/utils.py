439025b8b5494cf5467d8af51b1cd1c2241c6a97
44Scientifics <156848151+44Scientifics@users.noreply.github.com>
2025-04-17 06:26:04 +0000

    Add caching to get_all_cik function for improved performance

--- build/lib/FortyFour/Finance/utils.py
+++ build/lib/FortyFour/Finance/utils.py
@@ -5,7 +5,7 @@ import pandas as pd
 import plotly.express as px
 import logging
 
-
+@cache
 def get_all_cik():
     headers = {
         'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246",
