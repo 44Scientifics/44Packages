01828d5b74e4c7dd6d3dd33361acc3618719ef71
44Scientifics <156848151+44Scientifics@users.noreply.github.com>
2025-04-17 05:31:43 +0000

    Update get_all_cik to include 'ticker' in returned DataFrame and modify main execution to print results from get_all_cik

--- src/FortyFour/Finance/utils.py
+++ src/FortyFour/Finance/utils.py
@@ -43,7 +43,7 @@ def get_all_cik() -> pd.DataFrame:
     # Drop rows with missing values in 'cik' or 'NAME'
     df = df.dropna(subset=["cik", "NAME"])
 
-    return df[["cik", "NAME"]].reset_index(drop=True)
+    return df[["cik", "ticker", "NAME"]].reset_index(drop=True)
 
 
 def create_spark_line(data, _height: int = 100, _width: int = 250):
@@ -164,5 +164,5 @@ if __name__ == "__main__":
         print(f"Accounting norms found for {cik}: {accounting_norm_list}")
     else:
         print(f"No facts found in SEC response for {cik}.")
-    print(response)
+    print(get_all_cik())
 
